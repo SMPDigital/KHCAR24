c3ad5ce8f3eab05f5eacf2a76b2d3c98ceb91b97
OKAvishkaDilshan <avishka@smartannualreport.com>
2025-03-27 15:49:46 +0530

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2341,6 +2341,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    gsap.from(".ag-igib-counterbox", {
+        opacity: 0,
+        scale: 0.5,
+        duration: 1.5,
+        stagger: 0.4, // Animates each box one after another
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".ag-igib-counterbox-row",
+            start: "top bottom", // When the section enters the viewport
+            end: "bottom top",
+            toggleActions: "play none none reverse",
+        }
+    });
+});
+
 
 
 
